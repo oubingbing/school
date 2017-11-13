aba381a2e90dade5d3a32bb863b0638d60fc20db
xzf0587 <875307054@qq.com>
2017-11-13 14:55:30 +0800

    添加贴子相关的表

---
+++ database/migrations/2017_11_12_135848_create_inboxes_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInboxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inboxes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigIncrements('owner_id')->index()->comment('信箱的所有者');
+            $table->bigIncrements('college_id')->index()->nullable()->comment('评论所属的学校');
+
+            $table->string('content',1024)->comment('信箱的内容');
+
+            $table->bigInteger('obj_id')->index()->commment('对象的Id');
+            $table->tinyInteger('obj_type')->comment('对象的类型');
+            $table->tinyInteger('action_type')->comment('信箱的操作类型,例如发帖,评论,回复评论,点赞,关注');
+
+            $table->timestamp('post_at')->nullable()->comment('发送的时间');
+            $table->timestamp('read_at')->nullable()->comment('阅读的时间');
+
+            $table->timestamp('created_at')->nullable()->index();
+            $table->timestamp('updated_at')->nullable()->index();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inboxes');
+    }
+}
