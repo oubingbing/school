77f49cdd2e29ecdc4e6b44c484a73dd009e76264
xzf0587 <875307054@qq.com>
2018-01-21 18:34:38 +0800

    fix route error

--- app/Http/Controllers/Wechat/UserController.php
+++ app/Http/Controllers/Wechat/UserController.php
@@ -98,6 +98,8 @@ class UserController extends Controller
         $user = request()->input('user');
         $name = request()->input('school_name');
 
+        return $name;
+
         if (empty($name)) {
             throw new ApiException('内容不能为空', '50005');
         }
