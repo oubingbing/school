f3c91838d84b53398d6f9de225a9ed80fbda6195
xzf0587 <875307054@qq.com>
2018-03-17 20:14:59 +0800

    烟花过后自动跳转

--- public/js/flower.js
+++ public/js/flower.js
@@ -64,7 +64,7 @@ $(function() {
 	var listLaunchSound = $('audio.launch');
 
 	// define array position of text
-	var textString = 'happy';
+	var textString = 'happyl';
 	var textMatrix = [
 		4.5, 0, 5.5, 0, 6.5, 0, 7.5, 0, 8.5, 0,
 		0, 1, 1, 1, 2, 1, 3, 1, 4, 1, 6, 1, 7, 1, 8, 1, 10, 1, 11, 1, 12, 1, 13, 1,
@@ -88,10 +88,10 @@ $(function() {
 			0, 0, 0, 1, 1, 1, 1, 2, 1, 3, 2, 3, 2, 4, 2, 5, 2, 6, 2, 7,
 			3, 2, 3, 3, 4, 1, 4, 2, 5, 0, 5, 1
 		],
-		// l: [
-		// 	0, 0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7,
-		// 	1, 7, 2, 7, 3, 7, 4, 7, 5, 7
-		// ],
+		l: [
+			0, 0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7,
+			1, 7, 2, 7, 3, 7, 4, 7, 5, 7
+		],
 		// u: [
 		// 	0, 0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6,
 		// 	1, 7, 2, 7, 3, 7, 4, 7,
@@ -186,7 +186,7 @@ $(function() {
 
 		setTimeout(function () {
 			window.location.href = "https://kucaroom.com/hui_yi_de_cake";
-		}, 3000);
+		}, 2500);
 	}
 
 	function initSpark() {
