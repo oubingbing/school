946cf1160da3db9f24003f797d684884b1cc9c7a
xzf0587 <875307054@qq.com>
2018-03-18 22:52:38 +0800

    更改log日志的配置

--- config/app.php
+++ config/app.php
@@ -121,9 +121,11 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => env('APP_LOG', 'daily'),
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
+
+    'log_max_files' => 60,
 
     /*
     |--------------------------------------------------------------------------
