7d49f0d74afa465e787704f08920496d14c71adf
ouzhibing <ouzhibing@galaxymx.com>
2018-05-11 16:45:54 +0800

    长连接

--- resources/views/test/socket.blade.php
+++ resources/views/test/socket.blade.php
@@ -40,9 +40,18 @@
      * start_gateway.php 中需要指定websocket协议，像这样
      * $gateway = new Gateway(websocket://0.0.0.0:7272);
      */
+
+    console.log('start');
+
     ws = new WebSocket("wss://kucaroom.com:7272");
+
+    console.log('connect');
+
     // 服务端主动推送消息时会触发这里的onmessage
     ws.onmessage = function(e){
+
+        console.log('on message');
+
         // json数据转换成js对象
         var data = eval("("+e.data+")");
         var type = data.type || '';
@@ -50,7 +59,7 @@
             // Events.php中返回的init类型的消息，将client_id发给后台进行uid绑定
             case 'init':
                 // 利用jquery发起ajax请求，将client_id发给后端进行uid绑定
-                $.post('https://kucaroom.com/bind.php', {client_id: data.client_id}, function(data){}, 'json');
+                $.post('https://kucaroom.com/bind', {client_id: data.client_id}, function(data){}, 'json');
                 break;
             // 当mvc框架调用GatewayClient发消息时直接alert出来
             default :
